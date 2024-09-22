11cd692371d93edfdf0b20fcad23a322409e4999
julinhouu00 <juliocesar13b@gmail.com>
2024-09-22 14:20:20 -0300

    new commit

--- src/main/java/com/jbraga/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/jbraga/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,11 +1,15 @@
 package com.jbraga.minhasfinancas.api.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
 
     private String email;
