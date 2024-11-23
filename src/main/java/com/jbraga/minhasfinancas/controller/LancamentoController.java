d5c1cb966083b5874a21af0be78dc2e41bed4d8c
julinhouu00 <juliocesar13b@gmail.com>
2024-11-22 21:21:01 -0300

    new commit

--- src/main/java/com/jbraga/minhasfinancas/controller/LancamentoController.java
+++ src/main/java/com/jbraga/minhasfinancas/controller/LancamentoController.java
@@ -19,7 +19,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/lancamentos")
 @RequiredArgsConstructor
