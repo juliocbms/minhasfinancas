a0395c42345b41b55a03890553c6b135775b1ffe
RonanWandre <ronanwsdf@gmail.com>
2024-11-29 18:12:08 -0300

    implementando logs

--- src/main/java/com/jbraga/minhasfinancas/exception/RegraNegocioException.java
+++ src/main/java/com/jbraga/minhasfinancas/exception/RegraNegocioException.java
@@ -1,7 +1,14 @@
 package com.jbraga.minhasfinancas.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class RegraNegocioException extends RuntimeException {
+
+    private static final Logger logger = LoggerFactory.getLogger(RegraNegocioException.class);
+
     public RegraNegocioException(String msg) {
         super(msg);
+        logger.warn("Violação de regra de negócio: {}", msg); // Log de aviso
     }
 }
